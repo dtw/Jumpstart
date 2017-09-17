2d4d1e56478bc9c878f97aa23febf3824c5a1bc1
dtw <dtw@users.noreply.github.com>
2017-09-17 20:37:39 +0100

    Add code to hide nav on scroll.

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,6 +5,27 @@ $(document).ready(function () {
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
-    pauseOnHover: false,
+    pauseOnHover: false
+  });
+
+  var scrollDistanceFromTop = 0;
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    if ((scrollTop - scrollDistanceFromTop) >= 50) {
+      var navbarHeight = $('.navbar').height();
+      $('.navbar').animate({
+        top: 0 - navbarHeight
+      }, 150, function() {
+        // Animation complete.
+      });
+      scrollDistanceFromTop = scrollTop;
+    } else if ((scrollDistanceFromTop - scrollTop) >= 50){
+      $('.navbar').animate({
+        top: '0px'
+      }, 150, function() {
+        // Animation complete.
+      });
+      scrollDistanceFromTop = scrollTop;
+    };
   })
 })
